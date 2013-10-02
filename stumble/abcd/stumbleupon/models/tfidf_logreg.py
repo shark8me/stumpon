b456a9b95f446bdff1b3e0b61c215c3f8cf8d39b
Kiran Karkera <kiran.karkera@gmail.com>
2013-10-02 08:45:01 +0530

    got gridsearch results for build_m2

--- stumble/abcd/stumbleupon/models/tfidf_logreg.py
+++ stumble/abcd/stumbleupon/models/tfidf_logreg.py
@@ -28,15 +28,8 @@ def build_m2():
 
     pipeline = Pipeline([
         ('vect', CountVectorizer()),
-        ('tfidf', TfidfTransformer()),
-#        ('tfv', TfidfVectorizer(strip_accents='unicode',
- #                               use_idf=1,smooth_idf=1,
-  #                             analyzer='word',token_pattern=r'\w{1,}',
-#                                max_features=None)),        
+        ('tfidf', TfidfTransformer()),       
         ('clf', lm.SGDClassifier()),
-        #('logreg',lm.LogisticRegression(penalty='l2', dual=False, tol=0.001,
-        #                         C=1, fit_intercept=True, intercept_scaling=1.0,
-        #                         class_weight=None, random_state=None))
     ])
 
     parameters = {
@@ -47,14 +40,39 @@ def build_m2():
         'tfidf__norm': ('l1', 'l2'), #'l1'
         'clf__alpha': (0.00001, 0.000001), #best 1e-05
         'clf__penalty': ('l2', 'elasticnet'), #'l2'
-        #'tfv__min_df':(1,2,3),
-        #'tfv__ngram_range': ((1, 1), (1, 2)),
-        #'logreg__penalty':('l1','l2'), #l2 is the best
-        #'logreg__C':(0.1,0.5,1)
         'clf__n_iter': (10, 50, 80), #best 50
     }
     #pipe=build_tfidf_logreg();
     return pipeline,parameters
 
+def build_m3():
+    '''
+    Result:
+    done in 1230.728s
+()
+Best score: 0.809
+Best parameters set:
+    logreg__C: 1
+    logreg__penalty: 'l2'
+    tfv__min_df: 1
+    tfv__ngram_range: (1, 1)
+    '''
+    pipeline = Pipeline([
+        ('tfv', TfidfVectorizer(strip_accents='unicode',
+                               use_idf=1,smooth_idf=1,
+                               min_df=1,ngram_range=(1,1),
+                             analyzer='word',token_pattern=r'\w{1,}',
+                                max_features=None)),        
+        ('logreg',lm.LogisticRegression(penalty='l2', dual=False, tol=0.001,
+                                 C=1, fit_intercept=True, intercept_scaling=1.0,
+                                 class_weight=None, random_state=None))
+    ])
 
+    parameters = {
+        'tfv__min_df':(1,2,3),
+        'tfv__ngram_range': ((1, 1), (1, 2)),
+        'logreg__penalty':('l1','l2'), #l2 is the best
+        'logreg__C':(0.1,0.5,1)
+    }
+    return pipeline,parameters
 
